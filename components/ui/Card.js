a1516fab9d7e7f1a9280584e5bd526e5d65fd74a
Aditya Bhattad <adityabhattad18@gmail.com>
2022-11-28 12:22:08 +0530

    Initial Structure ✅

---
+++ components/ui/Card.js
@@ -0,0 +1,12 @@
+import clsses from "./Card.module.css";
+
+
+function Card(props){
+    return (
+        <div className={classes.card}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Card;
